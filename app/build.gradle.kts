a3e0878c8fdb6f1ff968affc15eaf1ed49d3c3a9
MSOB7YY <mohamedsobhy201566@gmail.com>
2026-08-12 04:30:14 +0200

    build: arm64 only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,15 @@ android {
     }
 
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("arm64-v8a")
+            isUniversalApk = false
+        }
+    }
+
     bundle {
         language {
             enableSplit = false
